1b1918ad624f97fd1492950a5c0ec6544806818f
2940cristian <2940cristian@gmail.com>
2018-08-13 14:08:03 -0700

    Testing Router
    Added component to test react-router. Also deleted the react icon

---
+++ front-end/src/Component/RoutingTest/RoutingTest.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Hello extends Component
+ {
+    constructor() {
+        super();
+    }
+
+    render() {
+      return (
+        <div>Component Routing test</div>
+      )
+    }
+  }
+
+export default Hello;
