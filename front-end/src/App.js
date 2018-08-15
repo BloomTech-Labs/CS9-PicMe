1537790e2dc1af9bd518d1c377d3c6c41e560d7c
Mark Wong <fmarkwong@gmail.com>
2018-08-14 21:22:42 -0700

    SignIn Page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Switch, Route} from "react-router-dom";
-import Hello from "./components/RoutingTest/RoutingTest.js"
+import SignIn from "./components/signin";
 
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={Hello}/>
+        <Route exact path="/" component={SignIn}/>
       </Switch>
     );
   }
