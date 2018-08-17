abef3622e271c8a49af4aaf5dff73fd5682cc99b
Mark Wong <fmarkwong@gmail.com>
2018-08-17 16:24:37 -0700

    Top nav bar layout

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 // import './App.css';
 import {Switch, Route} from 'react-router-dom'
-import Stripe from './components/stripe'
-import HomePage from './components/homepage.js'
-import Navbar from './components/navbar'
+import Billing from './components/billing'
+import HomePage from './components/homepage'
+import Layout from './components/top-nav-bar-layout'
 
 class App extends Component {
   render() {
@@ -11,8 +11,9 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/stripe" component={Stripe}/>
-          <Route exact path="/navbar" component={Navbar}/>
+          <Layout>
+            <Route exact path="/billing" component={Billing}/>
+          </Layout>
         </Switch>
       </div>
     );
