b40fd357c3b1c82bb8da848de28c06323954029b
2940cristian <2940cristian@gmail.com>
2018-08-17 10:33:48 -0700

    Updated backend update route so email is able to be changed as well

--- back-end/controllers/update.js
+++ back-end/controllers/update.js
@@ -4,10 +4,11 @@ const User = require("../db/models/user")(db, Sequelize);
 //Database initializers, used to make queries ^
 
 const update = (req, res) => {
+  const currEmail = req.body.currEmail //will grab current email if it is being changed
   const {email, password, first_name, last_name, nick_names, credits} = req.body;
   if (email) {
-    User.update({ password, first_name, last_name, nick_names, credits },
-      { where: { email: email }, individualHooks: true })
+    User.update({email, password, first_name, last_name, nick_names, credits },
+      { where: { email: currEmail }, individualHooks: true })
     .then(user => res.status(200).json({ success: true }))
     .catch(err => console.log(err))
   } else {
