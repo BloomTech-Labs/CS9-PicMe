501380d29ce14554ecb8d315b07f3bac94edc230
2940cristian <2940cristian@gmail.com>
2018-08-18 09:12:08 -0700

    working on icons

--- front-end/src/components/profile-settings.js
+++ front-end/src/components/profile-settings.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {Container, Header, Segment, Input, Button} from "semantic-ui-react";
 import "./css/profile-settings.css";
 // import Navbar from "../Navbar/Navbar"
+import Axios from "axios";
+import Closed from "./icons/closed.png";
+import Open from "./icons/open.png"
 
 
 class ProfileSettings extends Component {
@@ -23,6 +26,27 @@ class ProfileSettings extends Component {
         })
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const updatedUser = {
+            currEmail: "test@test.com",
+            first_name: this.state.firstName,
+            last_name: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password,
+            nick_names: this.state.nickname,
+            credits: 10
+        }
+
+        Axios.put("http://localhost:5000/update", updatedUser)
+        .then(response => {
+            console.log(response);
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
     //Post method will be here, server needs work first
 
 
@@ -30,14 +54,17 @@ class ProfileSettings extends Component {
         return(
             <div className="settings__nav">
                 {/* <Navbar/> */}
-                <form className="settings">
+                <form onSubmit={this.onSubmit} className="settings">
                     <Header as='h3' content='Edit Profile' style={style.h3} textAlign='center' />
                     <Container text>
                     <Segment.Group>
                         <Segment>First Name: <Input name="firstName" onChange={this.onChange} type="text"/></Segment>
                         <Segment>Last Name <Input name="lastName" onChange={this.onChange} type="text"/></Segment>
                         <Segment>Email: <Input name="email" onChange={this.onChange} type="text"/></Segment>
-                        <Segment>Password: <Input name="password" onChange={this.onChange} type="text"/></Segment>
+                        <Segment>Password: <Input name="password" type="password" onChange={this.onChange}/>
+                        <img src={Closed} alt="Password hidden" width="20px"/>
+                        <img src={Open} alt="Password hidden" width="20px"/>
+                        </Segment>
                         <Segment>Nickname: <Input name="nickname" onChange={this.onChange} type="text"/></Segment>
                     </Segment.Group>
                     <Button type="submit" content='Save' primary />
