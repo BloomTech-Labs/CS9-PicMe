32409d9ecd9177fb4aa9ca210da0f4dda17c0c8e
Chris <cjmarsh725@gmail.com>
2018-08-20 12:35:05 -0700

    Added backend for retrieving image collections

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -5,6 +5,7 @@ const {StripeCharge} = require("../controllers/StripeCharge")
 const {validateToken} = require("../controllers/validateToken")
 const {uploadImage} = require("../controllers/uploadImage")
 const {uploads} = require("../controllers/uploads")
+const {collection} = require("../controllers/collection")
 
 module.exports = server => {
     server.route('/signup').post(signup); //Export all routes from controllers
@@ -13,4 +14,5 @@ module.exports = server => {
     server.route('/charge').post(StripeCharge);
     server.route('/upload').get(uploadImage); //Will be a post request once client side can uploads
     server.route('/uploads').post(uploads);
+    server.route('/collection').post(collection);
   };
