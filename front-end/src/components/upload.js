9ce4be0a12dac0bee57f0e9015442aaa52c482cb
2940cristian <2940cristian@gmail.com>
2018-08-20 14:10:14 -0700

    Connected FE upload to BE, images are sent to cloudinary

--- front-end/src/components/upload.js
+++ front-end/src/components/upload.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Axios from "axios";
 
+
 class Upload extends Component{
     constructor() {
         super();
@@ -17,23 +18,29 @@ class Upload extends Component{
 
     onSubmit = (event) => {
         event.preventDefault();
-        const fd = new FormData();
-        fd.append("image", this.state.image, this.state.image.name);
-        console.log(fd)
+        const image = new FormData();
+        image.append("file", this.state.image) 
+        image.append("upload_preset", "u03iyxti") //Sends data as a file to our server
 
-        Axios.post("http://localhost:5000/upload", fd)
-        .then(response => {
-            console.log(response);
+        Axios({
+            url: "http://localhost:5000/upload",
+            method: "POST",
+            headers: {
+                "Content-Type": "multipart/form-data"
+            },
+            data: image
+        }).then(res => {
+            console.log(res);
         }).catch(err => {
-            console.log(err);
+            console.log(err)
         })
     }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.onSubmit}>
-                    <input type="file" onChange={this.handleFileUpload}/>
+                <form onSubmit={this.onSubmit} encType='multipart/form-data'>
+                    <input name="image" type="file" onChange={this.handleFileUpload}/>
                     <button type="submit">submit</button>
                 </form>
             </div>
