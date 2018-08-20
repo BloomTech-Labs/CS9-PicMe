ead6b1a6c29547db662391a12ab8da80ac65b56d
2940cristian <2940cristian@gmail.com>
2018-08-20 08:16:46 -0700

    Made cloudinary upload hard-coded until FE is connected

--- back-end/controllers/cloudinary.js
+++ back-end/controllers/cloudinary.js
@@ -2,6 +2,8 @@ const Cloudinary = (req, res) => {
     const cloudinary = require("cloudinary"); //Cloudinary npm package
     const image = req.body
 
+    console.log(image)
+    
     cloudinary.config({ 
         cloud_name: 'picme', 
         api_key: process.env.CLOUDINARY_KEY, //ENV variables given to us through cloudinary, check google sheets
@@ -9,7 +11,7 @@ const Cloudinary = (req, res) => {
     });  
   
 
-    cloudinary.uploader.upload(image,
+    cloudinary.uploader.upload("https://images.unsplash.com/photo-1534607287018-541c7d97748f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f8492e948d2ac5c91199a2623b1c42af&auto=format&fit=crop&w=925&q=80",
     //Image is default for now until it can be done client side
     function(result) {
         if(result.error) {
