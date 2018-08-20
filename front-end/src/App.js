0b7c4b5374661d99529d31046517f56f85d8a103
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-08-20 15:11:03 -0400

    Robert Hogan. Addition of photo browser component route is /browser

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 // import './App.css';
-import {Switch, Route} from 'react-router-dom'
-import Billing from './components/billing'
-import HomePage from './components/homepage'
-import Layout from './components/top-nav-bar-layout'
-import ProfileSettings from "./components/profile-settings"
+import {Switch, Route} from 'react-router-dom';
+import Billing from './components/billing';
+import HomePage from './components/homepage';
+import Layout from './components/top-nav-bar-layout';
+import ProfileSettings from "./components/profile-settings";
+import PhotoBrowser from './components/PhotoBrowser';
 class App extends Component {
   render() {
     return (
@@ -12,6 +13,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/settings" component={ProfileSettings}/>
+          <Route exact path="/browse" component={PhotoBrowser} />
           <Layout>
             <Route exact path="/billing" component={Billing}/>
           </Layout>
