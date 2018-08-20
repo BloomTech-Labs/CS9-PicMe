e05ee673c36d1b704e02024121fdd18daecaa024
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-08-20 14:41:11 -0400

    Robert Hogan. Basic addition of image gallery.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,15 +5,19 @@ import Billing from './components/billing'
 import HomePage from './components/homepage'
 import Layout from './components/top-nav-bar-layout'
 import ProfileSettings from "./components/profile-settings"
+import PhotosBrowser from './components/PhotosBrowser';
+
+
 class App extends Component {
   render() {
     return (
       <div>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/settings" component={ProfileSettings}/>
+          <Route exact path="/settings" component={ProfileSettings} />
+          <Route exact path="/browse" component={PhotosBrowser} />
           <Layout>
-            <Route exact path="/billing" component={Billing}/>
+            <Route exact path="/billing" component={Billing} />
           </Layout>
         </Switch>
       </div>
---
+++ front-end/src/components/PhotosBrowser.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import Gallery from 'react-photo-gallery';
+import selfieImages from './selfies-test-images';
+import './css/PhotosBrowser.css';
+
+export default class PhotosBrowser extends React.Component {
+    render() {
+    return (
+        <div className="component-container">
+        <div className="gallery-container">
+        <h2> Check out these photos! </h2>
+            <Gallery photos={PHOTO_SET} margin={12}/>
+        </div>
+        </div>
+    );
+    }
+}
+const PHOTO_SET = [
+  {
+    src: selfieImages[0].url,
+    width: 1,
+    height: 1
+  },
+  {
+    src: selfieImages[1].url,
+    width: 1,
+    height: 1
+  },
+  {
+    src: selfieImages[2].url,
+    width: 1,
+    height: 1
+  },
+  {
+    src: selfieImages[3].url,
+    width: 1,
+    height: 1
+  },
+  {
+    src: selfieImages[4].url,
+    width: 1,
+    height: 1
+  },
+  {
+    src: selfieImages[5].url,
+    width: 1,
+    height: 1
+  },
+  {
+    src: selfieImages[6].url,
+    width: 1,
+    height: 1
+  },
+  {
+    src: selfieImages[7].url,
+    width: 1,
+    height: 1
+  },
+  {
+    src: selfieImages[8].url,
+    width: 1,
+    height: 1
+  }
+];
