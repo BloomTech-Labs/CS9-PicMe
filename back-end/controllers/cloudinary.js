c3e84d83144f3c0df5d0e9a1c2221518fc6aecbc
2940cristian <2940cristian@gmail.com>
2018-08-20 08:20:23 -0700

    Added a comment

--- back-end/controllers/cloudinary.js
+++ back-end/controllers/cloudinary.js
@@ -10,7 +10,7 @@ const Cloudinary = (req, res) => {
         api_secret: process.env.CLOUDINARY_SECRET 
     });  
   
-
+    //Grabs from image link, will test grabbing image from fe later
     cloudinary.uploader.upload("https://images.unsplash.com/photo-1534607287018-541c7d97748f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f8492e948d2ac5c91199a2623b1c42af&auto=format&fit=crop&w=925&q=80",
     //Image is default for now until it can be done client side
     function(result) {
