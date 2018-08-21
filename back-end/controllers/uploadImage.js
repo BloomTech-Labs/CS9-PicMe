cdae026faaf704616022795a9232a72221a584fe
2940cristian <2940cristian@gmail.com>
2018-08-21 08:45:45 -0700

    Added comments and fixed small errors

--- back-end/controllers/uploadImage.js
+++ back-end/controllers/uploadImage.js
@@ -1,23 +1,19 @@
 const uploadImage = (req, res) => {
     const cloudinary = require("cloudinary"); //Cloudinary npm package
     const fs = require("fs");
-    let tags = [];
-    let nameTags;
+    let tags = []; //Tags we're passing onto the image
+    let nameTags; //Public id or mutliple id's we want to give tags to
 
+    //Recieves non-file info from our front-end, this is were we get out tags to upload
     req.busboy.on('field', function(fieldname, val) {
         if(fieldname === "Tags") {
-            console.log(fieldname, val);
-            // val.forEach(tag => {
-            //     tags.push(tag)
-            // })
             tags = val.split(",")            
         }
     });
     
     var fstream;
     req.pipe(req.busboy);
-    req.busboy.on('file', function (fieldname, file, filename) {
-        console.log("Uploading: " + filename); 
+    req.busboy.on('file', function (fieldname, file, filename) { //Grabs our file from FE and sets it into our images folder as cloudinary only likes paths
         fstream = fs.createWriteStream(__dirname + '/Images/' + filename);
         file.pipe(fstream);
     
@@ -30,20 +26,17 @@ const uploadImage = (req, res) => {
     cloudinary.uploader.upload(`controllers/Images/${filename}`, (result, error) => {
         if(error) {
             res.status(400).json({Err: "Failed to upload image"})
-            console.log(error)
         } else {
-            let file = (__dirname + '/Images/' + filename)
+            let file = (__dirname + '/Images/' + filename) //We delete the image from our server afterwards
             let deleteFile = fs.openSync(file, "r");
             fs.closeSync(deleteFile);
             fs.unlinkSync(file)
-            res.status(200).json({Message: "Success"})
-            console.log(result)
             nameTags = result.public_id
 
-            cloudinary.uploader.add_tag(tags, nameTags,
-            function(res, err) { 
-                if(res) console.log(res);
-                else console.log(err) 
+            cloudinary.uploader.add_tag(tags, nameTags, //Once image uploaded we add the desired tags to it
+            function(result, err) { 
+                if(result) res.status(200).json({Message: "Image uploaded"})
+                else res.status(500).json({Err: "Could not add tags at this time"})
             });
         }
     })
